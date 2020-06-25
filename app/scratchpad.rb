0a78eae8898d2aa05554217bb292a84f42b83718
Cody <cydoentis@gmail.com>
2020-06-25 14:37:56 -0400

    Fixed map generation

--- app/scratchpad.rb
+++ app/scratchpad.rb
@@ -159,10 +159,59 @@ def render_sprite args, x, y, sprite
     args.outputs.sprites << [x, y, args.state.tile_size, args.state.tile_size, sprite]
 end
 
+# Setup the initialize grid
+# def setup_grid args
+#     args.state.grid = args.state.grid_size.map do 
+#         args.state.grid_size.map { 0 } 
+#     end
+# end
+
+# def generate_tiles args
+#     for i in 0..args.state.grid_size do
+#         args.state.tiles[i] = []
+#         for j in 0..args.state.grid_size do
+#             if rand(0) < 0.3 || !in_bounds(args, i, j)
+#                 args.state.tiles[i][j] = Wall.new(i, j)
+#             else
+#                 args.state.tiles[i][j] = Floor.new(i, j)
+#             end
+#         end
+#     end
+# end
+
+# def setup_level args
+#     generate_tiles args
+#     render_level args
+# end
+
+# def in_bounds args, x, y
+#     return x > 0 && y > 0 && x < args.state.grid_size - 1 && y < args.state.grid_size - 1;
+# end
+
+# def get_tile args, x, y 
+#     if(in_bounds(args, x, y))
+#         return args.state.tiles[x][y]
+#     else
+#         return Wall.new(x, y)
+#     end
+# end
+
+# def render_level args
+#     for i in 0..args.state.grid_size do
+#         for j in 0..args.state.grid_size do
+#             get_tile(args, i, j).render_tile(args)
+#         end
+#     end
+# end
+
+
 # Generate a blank background -- "Off white color"
 def render_background args
     args.outputs.solids << [0, 0, 1280, 720, 255, 250, 240, 255]
 end
+
+
+# setup_level args
 
 # Main part of the program
 def tick args
